6831566fd15b20cb02ac1992441af82438ead3d4
MOHAMMED MOIN PASHA <MOHAMMED MOIN PASHA@DESKTOP-UEGFJ2E>
2025-03-12 11:30:30 +0530

    added open brower

--- Maven/src/test/java/com/easytrack/testscripts/campaingeModule.java
+++ Maven/src/test/java/com/easytrack/testscripts/campaingeModule.java
@@ -1,5 +1,10 @@
 package com.easytrack.testscripts;
 
+import java.time.Duration;
+
+import org.jspecify.annotations.Nullable;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.Reporter;
 import org.testng.annotations.Test;
 
@@ -9,5 +14,11 @@ public class campaingeModule {
 	@Test
 	public void Createmodule() {
 		Reporter.log("created module ", true );
+		WebDriver driver = new ChromeDriver() ;
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+		driver.get("https://www.google.co.in/");
+		String title = driver.getTitle();
+		Reporter.log(title , true);
 	}
 }
